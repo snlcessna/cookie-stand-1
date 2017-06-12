f68396271ad9c07635e8909b239ad996641d71d3
James Portune <Jayms.Portune@gmail.com>
2017-06-11 21:15:55 -0700

    Solved the style problem, able to add the JS to CSS styling

--- app.js
+++ app.js
@@ -1,16 +1,14 @@
-//function photoDim() {
-
-//}
-var img = new Image();
-var imgTest = ["photos/counter.jpg", "photos/deserts.jpg", "photos/fishcookies2.png", "photos/muffins.jpg", "photos/rolls.jpg"];
 debugger;
-  img.onload = function(){
-    for(var i = 0; i < imgTest.length ;i++){
-      var importImage = imgtest[i];
-      img.src = importImage;
-      height = img.height;
-      width = img.width;
-      console.log(height + " " + i);
-      console.log(width + " " + i);
+var imgTest = ["photos/counter.jpg", "photos/deserts.jpg", "photos/fishcookies2.png", "photos/muffins.jpg", "photos/rolls.jpg"];
+var img = new Image();
+
+window.onload = function(){ //Pulls the dimensions of each photo in the array imgTest to push to css for styling the images
+  for(var i = 0; i < imgTest.length ;i++){
+    var importImage = imgTest[i];
+    img.src = importImage;
+    height = img.height;
+    width = img.width;
+    document.documentElement.style.setProperty('--height', height + 'px');
+    document.documentElement.style.setProperty('--width', width + 'px');
+    }
   }
-}
