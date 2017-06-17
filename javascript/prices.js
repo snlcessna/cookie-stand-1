6cf812abb214f1b6efee4a7ca64cddd5cf156bb4
James <jayms.portune@gmail.com>
2017-06-17 07:47:10 -0700

    commit for lab turn in

--- javascript/prices.js
+++ javascript/prices.js
@@ -12,14 +12,15 @@ var storeData = {
   cxVisit: ranNum,
   hour: ['6am','7am','8am','9am','10am','11am','12pm','1pm','2pm','3pm','4pm','5pm','6pm','7pm']
 }
-for (var i = 0; i <= name.length; i++){
-  for (var j = 0; j<= hour.length; j++){
+for (var i = 0; i <= storeData.name.length; i++){
+  for (var j = 0; j<= storeData.hour.length; j++){
     var storeName = storeData.name(i);
     var ranCxNumber = storeData.cxVisit;
     var storeHour = storeData.hour(j);
+    console.log(storeName, ranCxNumber, storeHour);
   }
 }
-console.log(storeName, ranCxNumber, storeHour)
+
 
 function ranCxCount () {
   var ranCxCountPerHour = Math.floor(Math.random() * 20);
